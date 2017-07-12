342f4a4a85b4acea4235eaabe6b3ec0a19ded816
Wilson Lewis <wilsonlewis@gmail.com>
2017-07-11 20:46:44 -0400

    Add updating message

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <img src="./assets/logo.jpg">
+    <h1>
+      We have a new website!<br>
+      Stay tuned...
+    </h1>
   </div>
 </template>
 
@@ -12,6 +15,21 @@ export default {
 </script>
 
 <style>
+html {
+  background-color: #1A1A1A;
+  font-family: 'Old Standard TT', serif;
+  text-transform: uppercase;
+}
+
+
+h1 {
+  font-family: Helvetica, Arial, sans-serif;
+  color: #E6E6E6;
+  font-size: 22px;
+  margin: 60px 0;
+  line-height: 36px;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
